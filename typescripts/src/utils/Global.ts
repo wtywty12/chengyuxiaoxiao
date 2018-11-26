1c7cc777bd77698d28e350cacddc68209d32e605
zhanglin <996019649@qq.com>
2018-11-26 12:01:16 +0800

    拼欢乐成语UI

---
+++ typescripts/src/utils/Global.ts
@@ -0,0 +1,25 @@
+/**
+ * 存放全局变量
+ */
+export class _G {
+    /** 游戏类型  */
+    public static GameType = {
+        YouZiXiaoXiaoLe : 1,
+        HuanLeChengYu : 2,
+    }
+    /** 获取游戏类型  */
+    public static getGameType = _G.GameType.HuanLeChengYu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
